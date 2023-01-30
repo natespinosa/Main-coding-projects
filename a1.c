5568fe832f37c11275c7cf2f53772766ff65d150
natespinosa <123915856+natespinosa@users.noreply.github.com>
2023-01-30 02:07:00 -0500

    Add files via upload

---
+++ a1.c
@@ -0,0 +1,395 @@
+#include "a1.h"
+
+Restaurant* initialize_restaurant(char* name){ //pointing to struct
+  
+  //making space for the fields
+  Restaurant* restaurant = (Restaurant *)malloc(sizeof(Restaurant)); 
+
+  //assigning the values in restaurant; saying go the address of the name and assign this value
+  restaurant -> name = name;
+  restaurant -> menu = load_menu(MENU_FNAME); //little menu; only a place to store the whole thing
+  restaurant -> num_completed_orders = 0;
+  restaurant -> num_pending_orders = 0;
+
+  //pending oredrs initialized to an empty queue
+  Queue * new_queue = (Queue *)malloc(sizeof(Queue));
+
+  new_queue -> head = NULL;
+  new_queue -> tail = NULL;
+  
+  //initializing pending_orders to Queue with head and tail
+  restaurant -> pending_orders = new_queue; 
+
+  return restaurant;
+} //do i need to free in here ?
+
+
+
+
+Menu* load_menu(char* fname){
+  //first, open the file with the menu fname
+  FILE * fptr  = fopen(fname, "r");
+
+  int line_len = ITEM_CODE_LENGTH + MAX_ITEM_NAME_LENGTH; //varies with each item
+  char line[line_len]; //track of the # of item, info is kept here
+
+  int count = 0;
+
+  //count the number of lines (items)
+  //fgets(str, int, file) reads a line and stores it into the string
+  while(fgets(line, line_len, fptr)){ 
+    count++; //number of items
+  }
+
+  //assign the space for the variables, the "house"
+  char ** item_codes = (char **)malloc(count * sizeof(char *)); 
+  char ** item_names = (char **)malloc(count * sizeof(char *));
+  double * item_cost_per_unit = (double *) malloc(count * sizeof(double)); 
+
+  //get the pointer from the beginning
+  rewind(fptr);
+
+  char* end_ptr; //use later for strtod
+
+  //iterate through every line and "copy paste" the into to the variables
+  for(int i = 0; i < count; i++){
+    fgets(line, line_len, fptr); //fgets(str, int, file)
+
+    /*if(i == count -1){ //i think this is used when we are about to reach the end, not sure
+      line[strlen(line)] = ' ';
+    }*/
+
+    //strtok separates the string by the delimitation given, in this case MENU_DELIM
+    //malloc to assign the "rooms" inside the house
+  
+    //creating the size of each item code
+    item_codes[i] = (char*)malloc(sizeof(char) * ITEM_CODE_LENGTH);  
+    strcpy(item_codes[i], strtok(line, MENU_DELIM)); //assign the line value form the file
+
+    item_names[i] = (char*)malloc(sizeof(char) * MAX_ITEM_NAME_LENGTH);
+    strcpy(item_names[i], strtok(NULL, MENU_DELIM)); //ok wtf with NULL O_o 
+
+    //strtod converts the argument str to a floating-point number (type double) "(const char *str, char **endptr)"
+    item_cost_per_unit[i] = strtod(strtok(NULL, "$"), &end_ptr);
+  }
+  
+  fclose(fptr);
+
+  // creating the menu with the information recovered form the file
+  struct Menu * loaded_menu = (struct Menu *)malloc(sizeof(struct Menu));
+
+  loaded_menu -> num_items = count;
+  loaded_menu -> item_codes = item_codes;
+  loaded_menu -> item_names = item_names;
+  loaded_menu -> item_cost_per_unit = item_cost_per_unit;
+
+  return loaded_menu;
+}
+
+
+
+
+Order* build_order(char* items , char* quantities){
+ // Order* order = build_order("A1B1C1", "20,11,17");
+ // order ->num_items; // should be 3
+  int num_items; 
+  
+  if (items == NULL){ //if we have no items, 0 LOL
+    int num_items = 0;
+  }
+    
+  else {
+    num_items = strlen(items)/(ITEM_CODE_LENGTH - 1); 
+  }
+  
+  //making easy money with the space for these variables
+  char ** item_codes = (char **)malloc(sizeof(char *) * num_items); //creating [i]
+  int * item_quantities = (int *)malloc(sizeof(int) * num_items);
+    
+  char * temp_items = items;
+
+  int USABLE_LENGTH = ITEM_CODE_LENGTH - 1;
+  int j = 0;
+  int i = 0;
+  int l = 0;
+
+  //keep doing it while all the items are added
+  while(i < num_items){
+    //creating the size of each item code[j]
+    item_codes[i] = (char*)malloc(sizeof(char) * ITEM_CODE_LENGTH); 
+    char temp_quantities[MAX_ITEM_QUANTITY_DIGITS];
+    strcpy(temp_quantities, quantities);
+    
+    while(j < USABLE_LENGTH){ //i always increasing, always going to the next node
+      item_codes[i][j] = temp_items[l];  //creating the size of each item code
+      j++;
+      l++; //different from j, doughnut want to set to 0 again, string continues
+    }
+    
+    item_codes[i][USABLE_LENGTH] = '\0';
+    j = 0; //need to set to 0 in order to start again to count the length on next loop
+
+    if (i == 0){
+      item_quantities[i] = atoi(strtok(temp_quantities, MENU_DELIM));
+    }
+      
+    else {
+      item_quantities[i] = atoi(strtok(NULL, MENU_DELIM));
+    }
+    
+    i++; //next item
+  }
+
+  //creating the order and assign the info
+  struct Order * order = (struct Order *)malloc(sizeof(struct Order));
+  
+  order -> num_items = num_items;
+  order -> item_codes = item_codes;
+  order -> item_quantities = item_quantities;
+
+  return order;  
+}
+
+
+
+void enqueue_order(Order* order , Restaurant* restaurant ){
+
+  //create the cargo of QueueNode
+  struct QueueNode * NewOrder = (struct QueueNode*)malloc(sizeof(struct QueueNode));
+
+  //initialize the values, just enough without refering to the pending orders
+  NewOrder -> order = order;
+  NewOrder -> next = NULL;
+
+  //if the queue is empty
+  if(restaurant -> pending_orders -> tail == NULL){
+    //since is is the 1st node
+    restaurant -> pending_orders -> head = NewOrder;
+    restaurant -> pending_orders -> tail = NewOrder;
+  }
+
+  //how are we assigning the same varible to both?
+  else {
+    restaurant -> pending_orders -> tail -> next = NewOrder; //to create space? 
+    restaurant -> pending_orders -> tail = NewOrder;
+  }
+
+  (restaurant -> num_pending_orders++);
+}
+
+
+
+Order* dequeue_order(Restaurant* restaurant){
+  //return the dequeued order
+  //create the struct for the returning pointer
+  struct QueueNode* container = NULL;
+  struct Order * order_1 = NULL; 
+
+  //check if it's the last node
+  if(restaurant -> pending_orders -> head == restaurant -> pending_orders -> tail){
+    container = restaurant -> pending_orders -> head;
+    order_1 = container -> order;
+    restaurant -> pending_orders -> tail = NULL;
+    restaurant -> pending_orders -> head = NULL;
+  }
+
+  else {
+    container = restaurant -> pending_orders -> head;
+    order_1 =  container -> order;
+    restaurant -> pending_orders -> head = restaurant -> pending_orders -> head -> next;
+  }
+
+  restaurant -> num_pending_orders--;
+  restaurant -> num_completed_orders++;
+  free(container);
+  return order_1;
+}
+
+
+
+double get_item_cost(char* item_code , Menu* menu){
+  /*iterate thorugh the arrey of item_codes given by Menu, check if the item is equal 
+  to the item given, if so access its cost price and return it*/
+  int count = menu -> num_items;
+  int i = 0;
+  double cost;
+
+  for(i = 0; i < count; i++){
+    if (strcmp((menu -> item_codes[i]), item_code)==0){  
+      //menu -> item_cost_per_unit)[i];
+      return (menu -> item_cost_per_unit[i]); //hehe lol harcoding time
+    }
+  }
+  
+  return 0;
+}
+
+
+
+double get_order_subtotal(Order* order , Menu* menu){
+  //we are given an order that we can access from the menu, we have acess to items, codes and quantities
+  float subtotal = 0;
+  float item_cost = 0;
+  int num_items = 0;
+  int i = 0;
+
+  char item_code_1[ITEM_CODE_LENGTH];
+  
+  while(i < order -> num_items){
+    strcpy(item_code_1, (order -> item_codes)[i]);
+    item_cost = get_item_cost(item_code_1, menu);
+    num_items = (order -> item_quantities)[i];
+    subtotal += (num_items * item_cost);
+    i++;
+  }
+  
+  return subtotal;
+}
+
+
+
+double get_order_total(Order* order , Menu* menu){
+  double total = 0;
+  double taxes = 0;
+
+  //+1 (?)
+  taxes = (double)TAX_RATE/(double)100 + 1; //will round without double
+  total = get_order_subtotal(order, menu) * taxes;
+
+  return total;
+}
+
+
+
+int get_num_completed_orders(Restaurant* restaurant ){
+  int completed_orders = 0;
+  completed_orders = restaurant -> num_completed_orders;
+  return completed_orders;
+}
+
+
+
+int get_num_pending_orders(Restaurant* restaurant ){
+  int pending_orders = 0;
+  pending_orders = restaurant -> num_pending_orders;
+  return pending_orders;
+}
+
+
+
+void clear_order(Order ** order){
+  int i = 0; 
+  
+  while(i < (*order) -> num_items){
+    free((*order) -> item_codes[i]);
+    i++;
+  }
+  //free(item_codes);
+  //free(item_quantities);
+  free((*order) -> item_codes);
+  free((*order) -> item_quantities);
+  free(*order);
+
+  
+  (*order) = NULL;
+/*
+  free((*NewOrder));
+  (*NewOrder) = NULL;
+
+  free((*order_1));
+  (*order_1) = NULL;
+
+  free((*item_code_1));*/
+  //free(order);
+//free Qeueue nodes
+}
+
+
+
+void clear_menu(Menu** menu){
+  
+  for (int i = 0; i < (*menu)-> num_items; i++){
+    free((*menu) -> item_codes[i]);
+    free((*menu) -> item_names[i]);
+  }
+
+  //free((*menu) -> num_items);
+  free((*menu) -> item_names);
+  free((*menu) -> item_codes);
+  free((*menu) -> item_cost_per_unit);
+  
+  //free(loaded_menu);
+  free((*menu));
+  
+  (*menu) = NULL;
+  //free(menu);
+}
+
+
+void close_restaurant(Restaurant ** restaurant ){
+  clear_menu(&((*restaurant) -> menu));
+  
+  //while loop to free every QueueNode
+  
+  while (((*restaurant) -> pending_orders -> head) != NULL){
+    QueueNode* temp = (*restaurant) -> pending_orders -> head;
+    (*restaurant) -> pending_orders -> head = (*restaurant) -> pending_orders -> head -> next;
+    clear_order(&(temp -> order));
+    free(temp);
+    temp = NULL;
+  }
+  
+  free((*restaurant) -> pending_orders);
+  free(*restaurant);
+  
+  (*restaurant) = NULL;
+  
+  //free(restaurant);  
+}
+
+
+
+void print_menu(Menu* menu){
+	fprintf(stdout, "--- Menu ---\n");
+	for (int i = 0; i < menu->num_items; i++){
+		fprintf(stdout, "(%s) %s: %.2f\n", 
+			menu->item_codes[i], 
+			menu->item_names[i], 
+			menu->item_cost_per_unit[i]	
+		);
+	}
+}
+
+
+void print_order(Order* order){
+	for (int i = 0; i < order->num_items; i++){
+		fprintf(
+			stdout, 
+			"%d x (%s)\n", 
+			order->item_quantities[i], 
+			order->item_codes[i]
+		);
+	}
+}
+
+
+void print_receipt(Order* order, Menu* menu){
+	for (int i = 0; i < order->num_items; i++){
+		double item_cost = get_item_cost(order->item_codes[i], menu);
+		fprintf(
+			stdout, 
+			"%d x (%s)\n @$%.2f ea \t %.2f\n", 
+			order->item_quantities[i],
+			order->item_codes[i], 
+			item_cost,
+			item_cost * order->item_quantities[i]
+		);
+	}
+	double order_subtotal = get_order_subtotal(order, menu);
+	double order_total = get_order_total(order, menu);
+	
+	fprintf(stdout, "Subtotal: \t %.2f\n", order_subtotal);
+	fprintf(stdout, "               -------\n");
+	fprintf(stdout, "Tax %d%%: \t$%.2f\n", TAX_RATE, order_total);
+	fprintf(stdout, "              ========\n");
+}
---
+++ a2.c
@@ -0,0 +1,350 @@
+#include "a2.h"
+
+int bitwise_xor(int value){
+  return (KEY ^ value);
+}
+
+char *xor_encrypt(char c){    //pointer to binary
+
+    int decimal = bitwise_xor(c);
+    int i;
+    char *binary = malloc(sizeof(char)*8); // allocate space for a null terminating character as well
+
+    for(i = 0; i < 7; i++){
+      binary[i] = '0';
+    } i = 0;
+    // set the null terminating character
+    binary[7]='\0';
+
+    while(i < 7){
+        if((decimal % 2) == 1){
+            binary[6-i] = '1';
+        }
+        if ((decimal % 2)== 0){
+            binary[6-i] = '0';
+        }
+        decimal /= 2;
+        i++;
+    }
+    return binary;
+}
+
+char xor_decrypt(char *s){
+
+    int power = 1;
+    int decimal = 0;
+  
+    for(int i = 6; i >= 0; i--){
+      if(s[i] == '1'){
+        decimal += power;
+      }
+       power *=2;
+    }
+
+    decimal = (decimal ^ KEY);    
+    return decimal;
+}
+
+char *gen_code(char *msg){
+
+    int j, i = 0, l;
+    int passed_terminate = 0;
+
+    char * code = malloc(sizeof(char)*257);
+    code[256] = '\0';
+
+    //fill all the table to have a reference
+    for(j = 0;j <= 255; j++){
+        code[j] = '2';
+    } j = 0;
+
+    //fill with 0s  
+    for(j = 0;j <= 255; j++){
+        if(((j >= 17)&(j <= 19))||((j >= 28)&(j <= 30))||(j==33)||(j==35)||(j==44)||(j==46)||((j >= 49)&(j <= 51))||((j >= 60)&(j <= 62))||((j >= 192)&(j <= 194))||(j == 208)||(j == 210)||((j >= 225)&(j <= 227))){
+        code[j] = '0';
+        }
+    } j = 0;
+
+    //fill with 1s
+    for(j = 0;j <= 255; j++){
+        if((j <= 4)||((j >= 11)&(j <= 20)&(code[j] == '2'))||((j >= 27)&(j <= 36)&(code[j] == '2'))||((j >= 43)&(j <= 52)&(code[j] == '2'))||((j >= 59)&(j <= 68)&(code[j] == '2'))||((j >= 75)&(j <= 79))||((j >= 176)&(j <= 180)&(code[j] == '2'))||(j == 192)||(j == 196)||(j == 208)||(j == 210)||(j == 212)||(j == 224)||(j == 228)||((j >= 240)&(j <= 244))||(j == 255)){
+        code[j] = '1';
+        }
+    } j = 0;
+
+    passed_terminate = strlen(msg);
+    
+    // in the message, the final null-terminating character needs to be stored in the SC code - it cannot be assumed that
+    // a null-terminating character will be encrypted (and thus stored) as an ASCII value of 0. A string of seven zeros (post encryption)
+    // would indicate that the associated char (i.e. what you get when you decrypt the seven zeros) is equal to the KEY, which is not
+    // necessarily \0. As such, you need to actually perform an encryption on the null-terminating character and store it in the SC code.
+    // (also note, you need to store a null-terminating character at the end of the whole SC code, so there's actually 256 1s and 0s and then a 257th
+    // \0). So, when the message gets decrypted, it will include the null-terminating character so that if that message is read it is guaranteed
+    // to be a proper string in C.
+    while(i <= passed_terminate){ //for every character in the message, i counts the letters
+
+        char * en_msg = xor_encrypt(msg[i]); 
+        for(int m = 0; m <= 6; m++){ 
+            //here copies the string to the big array
+            //en_msg = realloc(en_msg, sizeof(char)*(l+1));
+            //l is taking the length of the entire message
+            while (j < 256){
+                if(code[j] == '2'){
+                    code[j] = en_msg[m];
+                    j=0;
+                    break;
+                }
+                j++;
+            } 
+        }
+        i++;
+        free(en_msg);
+    } i = 0; j = 0;
+    
+    //fill with code
+    while(code[j] != '\0'){
+        if(code[j] == '2'){
+            code[j] = '0';
+        }
+        j++;
+    } j = 0;
+    //printf("free: %s\n", en_msg);
+    //free(en_msg);
+    /*
+    while(j <= 255){
+        if(code[j] == '2'){
+            code[j] = '0';
+        }
+        j++;
+    }*/
+    return code;
+}
+
+char *read_code(char *code){
+    //add code here
+    /*
+    This function deciphers the message encoded in an SC code and generates a string containing
+    the deciphered message.
+    When the SC code in Appendix A is passed to scan code() as a single string input, a pointer
+    to the string "Program in C!" should be returned.
+    Input: <code> is a pointer to a 256-digit string of ‘1’s and ‘0’s, representing an SC code
+    containing an encrypted message.
+    Output: Return a pointer to a string containing the message obtained by deciphering the
+    input SC code.
+    */
+   char * en_str = malloc(sizeof(char)*181); 
+   int i = 0;
+   int j, l = 0;  
+
+    // en_str[180] = '\0';
+    // for(j = 0; j <= 179; j++){
+    //     en_str[j] = '-';
+    // }
+
+    //fill the boxes and get the string
+    for(j = 0;j <= 255; j++){
+        if((j <= 4)||(j >= 11)&(j <= 20)||(j >= 27)&(j <= 36)||(j >= 43)&(j <= 52)||(j >= 59)&(j <= 68)||(j >= 75)&(j <= 79)||(j >= 176)&(j <= 180)||(j >= 192)&(j <= 196)||(j >= 208)&(j <= 212)||(j >= 224)&(j <= 228)||(j >= 240)&(j <= 244)||(j == 255)){
+            code[j] = code[j];
+        }
+        else{
+            en_str[i] = code[j];
+            i++;
+        }
+    } i = 0; j = 0;
+
+    char deciphered [8];
+    char * final_str = malloc(sizeof(char)*27);
+    final_str[26]= '\0';
+    int m = 0;
+
+    for(j = 0; j <= 26; j++){
+        final_str[j] = '-';
+    } j = 0;
+
+    while(1){
+        for(j = 0; j <= 6; j++){
+            deciphered[j] = en_str[m + j];
+        }
+        deciphered[7] = '\0';
+        final_str[l] = xor_decrypt(deciphered);
+
+        if(final_str[l] == '\0'){
+             break;
+         }
+        m += 7;
+        l++;
+    }
+
+    free(en_str);
+    return final_str;
+    
+}
+
+char hexa_letter(int number){
+    char value;
+    if(number == 0){value = '0';}
+    if(number == 1){value = '1';}
+    if(number == 2){value = '2';}
+    if(number == 3){value = '3';}
+    if(number == 4){value = '4';}
+    if(number == 5){value = '5';}
+    if(number == 6){value = '6';}
+    if(number == 7){value = '7';}
+    if(number == 8){value = '8';}
+    if(number == 9){value = '9';}
+
+    if(number == 10){value = 'A';}
+    if(number == 11){value = 'B';}
+    if(number == 12){value = 'C';}
+    if(number == 13){value = 'D';}
+    if(number == 14){value = 'E';}
+    if(number == 15){value = 'F';}
+
+    return value;
+}
+
+char hexa_value(char *array){
+    char hexa;
+    int j, i=8, hexa_int=0;
+    for(j = 0; j <= 3; j++){ //this will be returning the sum of the 4 char array
+        if(array[j]=='1'){
+            hexa_int+=i;
+            i/=2;
+        }
+        else{
+            i/=2;
+        }
+    }
+    hexa = hexa_letter(hexa_int); //this will return the character of the 4 char array hopefully
+    return hexa;
+}
+
+char *compress(char *code){
+    /*
+    This function generates a string representing the results of converting the binary digits of
+    an SC code (‘1’s and ‘0’s) to hexadecimal digits (‘0’-‘9’, ‘A’-‘F’).
+    When the SC code in Appendix A is passed to compress() as a single string input,
+    the string "F83F8D11AAF58C71FA3F0B27DCDD3DE6439530000000F8008800A8008800F801"
+    should be returned.
+    Input: <code> is a pointer to a 256-digit string of ‘1’s and ‘0’s, representing an SC code.
+    Output: Return a pointer to a 64-digit string containing the hexadecimal representation
+    (‘0’-‘9’, ‘A’-‘F’) of the binary digits in <code>.
+    */
+    int j, m = 0;
+    int l = 0;
+    char * hexa_friend =malloc(sizeof(char)*6);
+    hexa_friend[5] = '\0';
+    char * hexa_val = malloc(sizeof(char)*65);
+    hexa_val[64] = '\0';
+
+
+    //for(int i = 0; i <= 62; i++){
+    while(m <= 63){
+        for(j = 0; j <= 4; j++){
+            if((code[j+l] == '1')||(code[j+l] == '0')){
+                hexa_friend[j] = code[j+l];
+            }
+        }   
+        hexa_friend[5] = '0'; 
+        hexa_val[m] = hexa_value(hexa_friend);
+        
+        m++;
+        l += 4;
+    }
+    free(hexa_friend);
+    return hexa_val;
+}
+
+char *decompress(char *code){
+    //add code here
+    /*
+    This function generates a string representing the results of converting the hexadecimal digits
+    (‘0’-‘9’, ‘A’-‘F’) of a compressed SC code to binary digits (‘1’s and ‘0’s).
+    decompress("F83F8D11AAF58C71FA3F0B27DCDD3DE6439530000000F8008800A8008800F801")
+    should return the SC code in Appendix A as a single 256-digit string of ‘1’s and ‘0’s.
+    Input: <code> is a pointer to a 64-digit string containing the hexadecimal representation
+    (‘0’-‘9’, ‘A’-‘F’) of a (compressed) SC code.
+    Output: Return a pointer to a 256-digit string of ‘1’s and ‘0’s, representing an SC code.
+    */
+        //printf("\n\n\nhello");
+
+    char * final_array = malloc(sizeof(char)*257);
+    int j, finalint, dec, ASCII, l;
+    final_array[256] = '\0';
+    for(int m = 0; m<=255; m++){
+        final_array[m] = '-';
+    }
+
+    for(j = 0; j<=63; j++){
+        //first convert the caharcter to decimal value
+        ASCII = code[j] - '\0';
+        //check wether we have a decimal or A-F based on ASCII   
+        if(ASCII <= 57){
+            dec = code[j] -'0';
+        }  
+        else { //if letter, convert to exact binary form Hexa
+            dec = code[j] - '7'; //decimal representation
+        } 
+        //printf("hex: %d\n", dec);
+        for(l = ((4*j)) + 3; l >= 4*j; l--){
+            finalint = dec % 2;
+            final_array[l]= finalint+'0';
+            dec = dec / 2;
+        //printf("array: %c, %d, %c\n", finalint+'0', l, final_array[l]);
+        }  
+        // printf("final array: %s", final_array);
+        // print_graph(final_array);
+    }
+    // print_graph(final_array);
+    // printf("final array: %s", final_array);
+    return final_array;
+}
+
+int calc_ld(char *sandy, char *cima){
+    //add code here
+    /*
+    calculate the length of both strings with a for loop, the matrix will have those dimentions
+    fill the first column from 0-lenght of each string -1
+    start filling based on constraints form left to right at each row (ie: (y*, #))
+    */
+    int x=0, y=0, i=0, min=0;
+    y = strlen(sandy);
+    x = strlen(cima);
+
+    //now we have 'matrix' dimensions
+    int matrix[x+1][y+1];
+    int col, row;
+
+    //iterate through each row and column and check the cases
+    for(col = 0; col <= y; col++){
+        for(row = 0; row <= x; row++){
+            //case 1 if S is an empty string
+            if(col == 0){
+                matrix[row][col] = row;
+            }
+            //case 2 if C is an empty string
+            else if(row == 0){
+                matrix[row][col] = col;
+            }
+            //case 3 S and C the same
+            else if (sandy[col-1] == cima[row-1]){
+                matrix[row][col] = matrix[row-1][col-1];
+            }
+            else{
+                min = 0;
+                if((matrix[row-1][col] < matrix[row-1][col-1])&&(matrix[row-1][col]<matrix[row][col-1])){
+                    min = matrix[row-1][col];
+                }
+                else if(matrix[row][col-1]<matrix[row-1][col-1]){
+                    min = matrix[row][col-1];
+                }
+                else{
+                    min = matrix[row-1][col-1];
+                }
+
+                matrix[row][col] = min+1;
+            }
+        }
+    }
+    return matrix[x][y];   
+}
